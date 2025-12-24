cb1b85693d9b13f3d7f3da249fd565e7a766282e
iamrafsanrafi <arafat.yasir1305@gmail.com>
2025-12-24 19:30:24 +0600

    Defined some nav menu options in navOptions.ts

---
+++ lib/navOptions.ts
@@ -0,0 +1,44 @@
+export const navOptions = [
+    {
+        id: 1,
+        name: "Tools",
+        type: "Submenu",
+        childrens: [
+            {
+                id: 1,
+                name: "Job Analyzer",
+                href: "/tools/job-analyzer"
+            },
+            {
+                id: 2,
+                name: "Resume Analyzer",
+                href: "/tools/resume-analyzer"
+            },
+            {
+                id: 3,
+                name: "Keyword Research",
+                href: "/tools/keyword-research"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "About",
+        url: "/about"
+    },
+    {
+        id: 3,
+        name: "Resources",
+        url: "/resources"
+    },
+    {
+        id: 4,
+        name: "Blogs",
+        url: "/blogs"
+    },
+    {
+        id: 5,
+        name: "Contact",
+        url: "/contact"
+    }
+];
