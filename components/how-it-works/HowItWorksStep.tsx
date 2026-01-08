e550a5ecf0527a8c065777ae8ebcaaf449c502e0
iamrafsanrafi <arafat.yasir1305@gmail.com>
2026-01-09 03:52:21 +0600

    Styled the connector with a step counter and changed section hover border color

--- components/how-it-works/HowItWorksStep.tsx
+++ components/how-it-works/HowItWorksStep.tsx
@@ -17,10 +17,14 @@ const HowItWorksStep = ({ step }: { step: Props }) => {
 
     return (
         <div className={cn(
-            "flex flex-col gap-10 lg:items-center justify-center border border-[rgb(var(--border-default))] hover:border-[rgb(var(--bg-primary-hover))] transition-all py-4 rounded-xl relative group",
+            `flex flex-col gap-10 lg:items-center justify-center border border-[rgb(var(--border-default))] hover:border-[rgb(var(--border-hover))] transition-all duration-250 py-4 rounded-xl relative group`,
             isEven ? "lg:flex-row-reverse" : "lg:flex-row",
-            
+
         )}>
+            <div className="absolute bottom-[calc(100%+60px)] left-1/2 -translate-x-1/2 translate-y-1/2 w-7 h-7 flex items-center justify-center text-background font-bold bg-[rgb(var(--bg-primary-hover))] z-20 rounded-full">
+                {id}
+            </div>
+            
             {/* ---- Section Connector ---- */}
             <div className="absolute bottom-[calc(100%+1px)] w-[2px] h-[120px] bg-linear-to-br from-[rgb(var(--bg-primary))]/20 to-[rgb(var(--bg-primary-hover))] z-10"></div>
 
@@ -29,17 +33,12 @@ const HowItWorksStep = ({ step }: { step: Props }) => {
                 "w-1/2 space-y-6",
                 isEven ? "text-right" : "text-left"
             )}>
-                {/* ---- Step Indicator ---- */}
-                <TextBadge>
-                    • Step {id}
-                </TextBadge>
-
-                <h3 className="text-3xl md:text-4xl font-bold tracking-tight text-foreground group-hover:text-[rgb(var(--bg-primary-hover))] transition-all">
+                <h3 className="text-3xl md:text-4xl font-bold tracking-tight text-foreground group-hover:text-[rgb(var(--bg-primary-hover))] transition-all duration-250">
                     {title}
                 </h3>
 
                 {/* ---- Main Description ---- */}
-                <div className="text-lg text-muted-foreground leading-relaxed [&_a]:text-[rgb(var(--bg-primary))] [&_a]:font-medium [&_a]:hover:underline [&_a]:hover:text-[rgb(var(--bg-primary-hover))] group-hover:text-foreground transition-all">
+                <div className="text-lg text-muted-foreground leading-relaxed [&_a]:text-[rgb(var(--bg-primary))] [&_a]:font-medium [&_a]:hover:underline [&_a]:hover:text-[rgb(var(--bg-primary-hover))] group-hover:text-foreground transition-all duration-250">
                     {description}
                 </div>
             </div>
